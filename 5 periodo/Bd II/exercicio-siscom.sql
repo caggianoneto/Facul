9d86fc4c065dbf343daa5093fc827da4374edb0d
caggianoneto <caggianoneto@gmail.com>
2016-05-22 17:10:51 -0300

    Tabela trabav2 pronta e preenchida

---
+++ 5 periodo/Bd II/exercicio-siscom.sql
@@ -0,0 +1,104 @@
+
+Utilizando o Sistema Comercial, responder esta lista de exercícios.
+
+Obs: se você achar necessário, incluir mais clientes com seus dados, mais vendas para outros
+	cliente ainda não vendidos ou os mesmos a quem já foi feita venda, incluir itens vendidos,
+	mais produtos, mais categorias de produtos e outros dados que achar necessário para validar
+	as instruções que estão sendo pedidas nesta lista.
+
+1. Mostrar o nome e o endereço de todas as pessoas (cliente) que moram na região sul do Brasil.
+
+2. Mostrar o nome de todas as pessoas que estão com o endereço ou o CEP nulos.
+
+3. Mostrar o nome, o CPF sem formatação e o CPF formatado (com pontuação) de todos os clientes.
+
+4. Mostrar o nome das pessoas que tem menos de 15 letras no nome.
+
+5. Mostrar a sigla do estado e a quantidade de pessoas em cada estado. Mostrar ordenado do maior número para o menor.
+
+6. Mostrar o nome, a data de nascimento e a idade de cada pessoa. Mostrar ordenado do mais velho para o mais novo.
+
+7. Mostrar o nome, a data de nascimento e o nome do mês de cada pessoa.
+
+8. Mostrar uma lista com o perfil por faixa etária de todas as pessoas cadastradas. Mostrar com a seguinte faixa etária:
++----------------+--------+
+|  faixa etária  |  qtde  |
++----------------+--------+
+|  0  - 20 anos  |   n    |
++----------------+--------+
+|  21 - 30 anos  |   n    |
++----------------+--------+
+|  31 - 40 anos  |   n    |
++----------------+--------+
+|  41 - 50 anos  |   n    |
++----------------+--------+
+|acima de 50 anos|   n    |
++----------------+--------+
+
+
+9. Mostrar o nome da categoria e a quantidade de produtos cadastrados para cada uma. Obs: para as
+	categorias que não tem nenhum produto cadastrado não é necessário mostrá-la.
+
+10. Mostrar o nome da categoria e a quantidade de produtos cadastrados para cada uma. Obs: para as
+	categorias que não tem nenhum produto cadastrado, mostrar 0, mas a categoria deve ser mostrada.
+
+11. Mostrar o nome do produto, a quantidade de que possui em estoque, o valor unitário do produto e
+	o valor total que tem em estoque. Obs: o valor total é a multiplicação da quantidade em estoque
+	pelo valor unitário.
+
+
+12. Mostrar o nome da categoria e o valor que possui em estoque. Obs: O valor que possui em estoque
+	é a soma da multiplicação da quantidade em estoque pelo valor unitário de cada produto. As
+	categorias que não possuem produto não é necessário mostrá-la.
+
+13. Mostrar o Id. da Venda, o nome do cliente e a data da realização.
+
+14. Mostrar o Id. da venda, o nome do cliente, o mês e o ano separados. Mostrar da venda mais nova
+	para a mais antiga. Obs: Mostrar ordenado primeiro pelo ano decrescente depois pelo mês
+	decrescente
+
+15. Mostrar o nome e quantidade de cada produto vendido. Obs: mostrar independente da data de venda
+	e se o produto foi vendido mais de uma vez, neste deve ser mostrada toda a quantidade vendida
+	do produto.
+
+16. Mostrar o nome e soma de cada produto vendido.
+
+17. Mostrar o nome e multiplicação venda de cada produto vendido. Mostrar ordenado pelo produto
+	que mais gerou receita.
+
+18. Mostrar o nome e multiplicação venda de cada produto vendido. Mostrar ordenado pelo produto
+	que mais gerou receita. Mostrar somente das vendas realizadas no mês de setembro de 2014.
+
+19. Mostrar o nome e multiplicação venda de cada produto vendido. Mostrar ordenado pelo produto
+	que mais gerou receita. Mostrar somente das vendas realizadas último mês que teve venda
+	realizada. Obs: atenção porque deve selecionar qual foi o último mês e último ano de venda,
+	que pode não ser o mês de setembro de 2014 (se for inserido mais vendas).
+
+20. Mostrar o nome de todos os produtos que nunca foram vendidos.
+
+21. Mostrar última data de venda de cada produto vendido.
+
+22. Para cada Item vendido, mostrar o Id. da venda, o nome do produto, o valor unitário e 
+	o valor total de cada item (a multiplicação da quantidade pelo valor unitário).
+
+23. Mostrar o perfil dos seus clientes por faixa etária (a mesma faixa da pergunta anterior).
+	Obs: este perfil por faixa etária é para mostrar quais as faixas etárias que mais compraram
+	em seu banco de dados.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
